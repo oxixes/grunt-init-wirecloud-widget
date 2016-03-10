e996a1db208970cdff1b4a72f42e69dc3b58922b
Álvaro Arranz <aarranz@fi.upm.es>
2016-03-10 15:37:36 +0100

    Improve Gruntfile.js license

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,17 +1,9 @@
-/*!
- *   Copyright 2014-2015 CoNWeT Lab., Universidad Politecnica de Madrid
+/*
+ * {%= name %}
+ * {%= homepage %}
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Copyright (c) {%= grunt.template.today('yyyy') %} {%= vendor_title %}
+ * Licensed under the {%= licenses.join(', ') %} license{%= licenses.length === 1 ? '' : 's' %}.
  */
 
 module.exports = function (grunt) {

c46eadd4eb7470ef7f05b258086e100fc0307eab
Álvaro Arranz <aarranz@fi.upm.es>
2016-03-29 12:21:41 +0200

    Fix problem adding the private field inside the package.json file

--- template.js
+++ template.js
@@ -198,11 +198,10 @@ exports.template = function(grunt, init, done) {
         // Write package.json :)
         var nobj = {};
         nobj.name = props.name;
-        nobj.private = true;
-        nobj.vendor = props.vendor;
         nobj.description = "This package.json file is only used for installing npm dependencies. But this is not an installable node package, but a WireCloud widget. Take a look into src/config.xml for more details about this widget"
         nobj.devDependencies = devDependencies;
         init.writePackageJSON('package.json', nobj, function(pkg, props) {
+            pkg.private = true;
             pkg.vendor = props.vendor;
             return pkg;
         });
